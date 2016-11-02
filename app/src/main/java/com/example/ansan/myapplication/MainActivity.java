39a07ba4b54527f088a3e39bc250169ae82a9e65
hanjuyeon <hanjuyeon95@naver.com>
2016-11-02 12:46:05 +0900

    폴더 생성 삭제 기능추가

--- app/src/main/java/com/example/ansan/myapplication/MainActivity.java
+++ app/src/main/java/com/example/ansan/myapplication/MainActivity.java
@@ -45,15 +45,15 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(getApplicationContext(), "폴더 삭제",Toast.LENGTH_SHORT).show();
                 break;
             case R.id.button3: // 파일 생성
-                myfolder.mkdir();
+
                 Toast.makeText(getApplicationContext(), "파일 생성",Toast.LENGTH_SHORT).show();
                 break;
             case R.id.button4: // 파일 읽기
-                myfolder.mkdir();
+
                 Toast.makeText(getApplicationContext(), "파일 읽기",Toast.LENGTH_SHORT).show();
                 break;
             case R.id.button5:  //파일 목록 가져오기
-                myfolder.mkdir();
+
                 Toast.makeText(getApplicationContext(), "파일 목록 가져오기",Toast.LENGTH_SHORT).show();
                 break;
 
